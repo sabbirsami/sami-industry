9da5a36150839b2bd0e52115330aa86b20308dde
SabbirSami <smd71430@gmail.com>
2022-05-21 23:15:10 +0600

    install react router, firebase, firebase hook,react bootstrap, hot tostify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <button className="btn btn-danger">bbb</button>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Toaster position="top-center" reverseOrder={false} />
+        </>
     );
 }
 
