b95827fd474c9a9bb5fe47e86159729a86412859
SabbirSami <smd71430@gmail.com>
2022-05-22 16:28:03 +0600

    simple sign up page setup done and fix some in login page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Products from "../src/Components/Home/Products";
 import Footer from "./Shared/Footer";
 import "swiper/css";
 import Login from "./Components/Login/Login";
+import SignUp from "./Components/Login/SignUp";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" element={<Home></Home>}></Route>
                 <Route path="/products" element={<Products></Products>}></Route>
                 <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/signup" element={<SignUp></SignUp>}></Route>
             </Routes>
             <Footer></Footer>
             <Toaster position="top-center" reverseOrder={false} />
--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -2,15 +2,23 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
     return (
-        <div>
+        <div className="pt-5">
             <div className="container">
                 <div className="m-5">
                     <div className="p-lg-5">
                         <div className="m-lg-5">
-                            <h1 className="text-center">Login</h1>
+                            <h1 className="text-center">Sign Up</h1>
                             <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Your Name</Form.Label>
+                                    <Form.Control
+                                        className="rounded-0"
+                                        type="text"
+                                        placeholder="Enter your name"
+                                    />
+                                </Form.Group>
                                 <Form.Group
                                     className="mb-3"
                                     controlId="formBasicEmail"
@@ -39,12 +47,12 @@ const Login = () => {
                                     type="submit"
                                     className="btn btn-danger rounded-0 w-100 py-2"
                                 >
-                                    Login
+                                    Sign Up
                                 </button>
                             </Form>
                             <p className="text-center pt-3">
-                                Dont have any account?{" "}
-                                <Link to="/signup">Sign Up</Link>
+                                Already have an account?{" "}
+                                <Link to="/login">Login</Link>
                             </p>
                             <div className="divide d-flex align-items-center pb-3">
                                 <div></div>
@@ -67,7 +75,7 @@ const Login = () => {
                                         <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
                                     </svg>
                                 </span>{" "}
-                                Login with Google
+                                Sign Up with Google
                             </button>
                         </div>
                     </div>
@@ -77,4 +85,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
