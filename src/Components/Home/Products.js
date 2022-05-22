93c8a222875e1147b057138745d5e76a440e8afe
SabbirSami <smd71430@gmail.com>
2022-05-22 20:51:01 +0600

    remove fake data and replace localhost link and fix some isshu

--- src/Components/Home/Products.js
+++ src/Components/Home/Products.js
@@ -4,17 +4,19 @@ import Product from "./Product";
 const Products = () => {
     const [products, setProducts] = useProducts([]);
     return (
-        <div>
+        <div className="pb-5">
             <div className="container">
                 <div className="row">
-                    <div className="pt-5 pb-4">
-                        <div className="product_content pt-5">
-                            <p className="text-danger text-center fs-5">
-                                OUR PRODUCTS
-                            </p>
-                            <h1 className="text-center pb-5  w-50 w-sm-0 px-5 mx-auto">
-                                We provide all of your industrial solution
-                            </h1>
+                    <div className="pt-lg-5">
+                        <div className="pt-5 pb-4">
+                            <div className="product_content pt-5">
+                                <p className="text-danger text-center fs-5">
+                                    OUR PRODUCTS
+                                </p>
+                                <h1 className="text-center pb-5  w-50 w-sm-0 px-5 mx-auto">
+                                    We provide all of your industrial solution
+                                </h1>
+                            </div>
                         </div>
                     </div>
                     {products.map((product) => (
