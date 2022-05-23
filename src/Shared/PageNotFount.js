28aaaca950d0937b84c8e0987f331e0e4fd3b4f6
SabbirSami <smd71430@gmail.com>
2022-05-23 13:42:26 +0600

    added 404 page

---
+++ src/Shared/PageNotFount.js
@@ -0,0 +1,12 @@
+import React from "react";
+import notFound from "../../src/Assets/images/notFound.png";
+
+const PageNotFount = () => {
+    return (
+        <div>
+            <img src={notFound} alt="" />
+        </div>
+    );
+};
+
+export default PageNotFount;
