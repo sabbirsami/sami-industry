57d491ef038650486f7a4697db910fc9ba715caf
SabbirSami <smd71430@gmail.com>
2022-05-24 00:19:23 +0600

    added requireAdmin

--- src/Hooks/useAdmin.js
+++ src/Hooks/useAdmin.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 
 const useAdmin = (user) => {
     const [admin, setAdmin] = useState(false);
+
+    // CUSTOM LOADING
+    const [adminLoading, setAdminLoading] = useState(true);
     useEffect(() => {
         const email = user?.email;
         if (email) {
@@ -17,9 +20,10 @@ const useAdmin = (user) => {
                 .then((res) => res.json())
                 .then((data) => {
                     setAdmin(data.admin);
+                    setAdminLoading(false);
                 });
         }
     }, [user]);
-    return [admin];
+    return [admin, adminLoading];
 };
 export default useAdmin;
---
+++ src/Shared/RequireAdmin.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate, useLocation } from "react-router-dom";
+import auth from "../firebase.init";
+import useAdmin from "../Hooks/useAdmin";
+import Loading from "./Loading";
+
+const RequireAdmin = ({ children }) => {
+    let location = useLocation();
+    const [user, loading, error] = useAuthState(auth);
+    const [admin, adminLoading] = useAdmin(user);
+
+    if (loading || adminLoading) {
+        return <Loading></Loading>;
+    }
+    if (!user || admin) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+    if (error) {
+        console.log(error);
+    }
+    return children;
+};
+
+export default RequireAdmin;
