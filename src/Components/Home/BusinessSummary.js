29077a8a718a9d9f11fee0d95b4aee752d611f97
SabbirSami <smd71430@gmail.com>
2022-05-23 14:47:08 +0600

    fix business section

--- src/Components/Home/BusinessSummary.js
+++ src/Components/Home/BusinessSummary.js
@@ -6,62 +6,84 @@ const BusinessSummary = () => {
     return (
         <div className="pb-3">
             <div className="container">
-                <div className="py-lg-5 py-2">
+                <div className="py-lg-4 py-2">
                     <div className="product_content pt-5">
+                        <h1 className="text-center w-lg-50 w-sm-0 mx-auto">
+                            Millions Business Trust Us
+                        </h1>
                         <p className="text-danger text-center fs-5">
                             USERS EXPECTATION
                         </p>
-                        <h1 className="text-center pb-1 w-lg-50 w-sm-0 mx-auto">
-                            Millions Business Trust Us
-                        </h1>
-                        <div className="divider mx-auto bg-danger"></div>
                     </div>
-                    <div className="business_section py-5 ">
+                    <div className="business_section py-4 ">
                         <div className="business_content py-4  ">
                             <div className="row">
-                                <div className="col-lg-4">
-                                    <div className="justify-content-center text-center">
+                                <div className="col-lg-3">
+                                    <div className=" d-flex justify-content-center align-items-center text-center">
                                         <FontAwesomeIcon
                                             icon={faFlag}
                                             style={{
                                                 color: "#d63031",
-                                                fontSize: "70px",
+                                                fontSize: "60px",
                                             }}
                                         />
-                                        <h1 className="pt-4 m-0">32+</h1>
-                                        <p className="m-0 p-0 text-danger">
-                                            Countries
-                                        </p>
+                                        <div className="text ps-3">
+                                            <h1 className=" m-0">32+</h1>
+                                            <p className="m-0 p-0 text-danger">
+                                                Countries
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-lg-4">
-                                    <div className="justify-content-center text-center">
+                                <div className="col-lg-3">
+                                    <div className=" d-flex justify-content-center align-items-center text-center">
                                         <FontAwesomeIcon
                                             icon={faThumbsUp}
                                             style={{
                                                 color: "#d63031",
-                                                fontSize: "70px",
+                                                fontSize: "60px",
                                             }}
                                         />
-                                        <h1 className="pt-4 m-0">432+</h1>
-                                        <p className="m-0 p-0 text-danger">
-                                            Feedbacks
-                                        </p>
+                                        <div className="text ps-3">
+                                            <h1 className=" m-0">32+</h1>
+                                            <p className="m-0 p-0 text-danger">
+                                                Feedbacks
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="col-lg-4">
-                                    <div className="justify-content-center text-center">
+                                <div className="col-lg-3">
+                                    <div className=" d-flex justify-content-center align-items-center text-center">
+                                        <FontAwesomeIcon
+                                            icon={faThumbsUp}
+                                            style={{
+                                                color: "#d63031",
+                                                fontSize: "60px",
+                                            }}
+                                        />
+                                        <div className="text ps-3">
+                                            <h1 className=" m-0">32+</h1>
+                                            <p className="m-0 p-0 text-danger">
+                                                Feedbacks
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-3">
+                                    <div className=" d-flex justify-content-center align-items-center text-center">
                                         <FontAwesomeIcon
                                             icon={faUsers}
                                             style={{
                                                 color: "#d63031",
-                                                fontSize: "70px",
+                                                fontSize: "60px",
                                             }}
                                         />
-                                        <h1 className="pt-4 m-0">273+</h1>
-                                        <p className="m-0 p-0 text-danger">
-                                            Happy Clients
-                                        </p>
+                                        <div className="text ps-3">
+                                            <h1 className=" m-0">32+</h1>
+                                            <p className="m-0 p-0 text-danger">
+                                                Happy Clients
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
