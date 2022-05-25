d30e13c94c7419bb53ae7be82673ab167fb874c1
SabbirSami <smd71430@gmail.com>
2022-05-26 01:06:22 +0600

    review section done

--- src/Components/AddReview/AddReview.js
+++ src/Components/AddReview/AddReview.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import addReviewSectionBg from "../../Assets/images/5405439.jpg";
 import { Form } from "react-bootstrap";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useForm } from "react-hook-form";
@@ -39,135 +39,134 @@ const AddReview = () => {
     return (
         <div className="add_review_section_bg">
             <div className="container">
-                <div className="row">
-                    <div className="my-lg-5">
-                        <div className="py-lg-5">
-                            <div className="py-5">
-                                <div className="col-lg-5">
-                                    <div className="m-lg-5">
-                                        <h1 className="text-center">
-                                            Add Review
-                                        </h1>
-                                        <div className="py-4">
-                                            <Form
-                                                onSubmit={handleSubmit(
-                                                    onSubmit
-                                                )}
+                <div className="pt-lg-4">
+                    <div className="pt-lg-5">
+                        <div className="row">
+                            <div className="col-lg-5">
+                                <div className="m-lg-5">
+                                    <h1 className="text-center">Add Review</h1>
+                                    <div className="py-4">
+                                        <Form onSubmit={handleSubmit(onSubmit)}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>
+                                                    Your Name
+                                                </Form.Label>
+                                                <Form.Control
+                                                    {...register("name", {
+                                                        required: true,
+                                                    })}
+                                                    readOnly
+                                                    value={displayName}
+                                                    className="rounded-0"
+                                                    type="text"
+                                                    placeholder="Enter name"
+                                                />
+                                            </Form.Group>
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicEmail"
                                             >
-                                                <Form.Group className="mb-3">
-                                                    <Form.Label>
-                                                        Your Name
-                                                    </Form.Label>
-                                                    <Form.Control
-                                                        {...register("name", {
-                                                            required: true,
-                                                        })}
-                                                        readOnly
-                                                        value={displayName}
-                                                        className="rounded-0"
-                                                        type="text"
-                                                        placeholder="Enter name"
-                                                    />
-                                                </Form.Group>
-                                                <Form.Group
-                                                    className="mb-3"
-                                                    controlId="formBasicEmail"
-                                                >
-                                                    <Form.Label>
-                                                        Email address
-                                                    </Form.Label>
-                                                    <Form.Control
-                                                        {...register("email", {
-                                                            required: true,
-                                                        })}
-                                                        value={email}
-                                                        readOnly
-                                                        required
-                                                        className="rounded-0"
-                                                        type="email"
-                                                        placeholder="Enter email"
-                                                    />
-                                                </Form.Group>
-                                                <div>
-                                                    {[...Array(5)].map(
-                                                        (star, index) => {
-                                                            const ratingValue =
-                                                                index + 1;
-                                                            return (
-                                                                <label>
-                                                                    <input
-                                                                        type="radio"
-                                                                        name="rating"
-                                                                        value={
+                                                <Form.Label>
+                                                    Email address
+                                                </Form.Label>
+                                                <Form.Control
+                                                    {...register("email", {
+                                                        required: true,
+                                                    })}
+                                                    value={email}
+                                                    readOnly
+                                                    required
+                                                    className="rounded-0"
+                                                    type="email"
+                                                    placeholder="Enter email"
+                                                />
+                                            </Form.Group>
+                                            <div>
+                                                {[...Array(5)].map(
+                                                    (star, index) => {
+                                                        const ratingValue =
+                                                            index + 1;
+                                                        return (
+                                                            <label>
+                                                                <input
+                                                                    type="radio"
+                                                                    name="rating"
+                                                                    value={
+                                                                        ratingValue
+                                                                    }
+                                                                    {...register(
+                                                                        "ratingValue",
+                                                                        {
+                                                                            required: true,
+                                                                        }
+                                                                    )}
+                                                                    onClick={() =>
+                                                                        setRating(
                                                                             ratingValue
-                                                                        }
-                                                                        {...register(
-                                                                            "ratingValue",
-                                                                            {
-                                                                                required: true,
-                                                                            }
-                                                                        )}
-                                                                        onClick={() =>
-                                                                            setRating(
-                                                                                ratingValue
-                                                                            )
-                                                                        }
-                                                                    />
-                                                                    <FontAwesomeIcon
-                                                                        className="star"
-                                                                        icon={
-                                                                            faStar
-                                                                        }
-                                                                        color={
-                                                                            ratingValue <=
-                                                                            (hoverRating ||
-                                                                                rating)
-                                                                                ? "#ffc107"
-                                                                                : "#e4e5e9"
-                                                                        }
-                                                                        onMouseOver={() =>
-                                                                            setHoverRating(
-                                                                                ratingValue
-                                                                            )
-                                                                        }
-                                                                        onMouseOut={() =>
-                                                                            setHoverRating(
-                                                                                null
-                                                                            )
-                                                                        }
-                                                                    />
-                                                                </label>
-                                                            );
-                                                        }
-                                                    )}
-                                                </div>
+                                                                        )
+                                                                    }
+                                                                />
+                                                                <FontAwesomeIcon
+                                                                    className="star"
+                                                                    icon={
+                                                                        faStar
+                                                                    }
+                                                                    color={
+                                                                        ratingValue <=
+                                                                        (hoverRating ||
+                                                                            rating)
+                                                                            ? "#ffc107"
+                                                                            : "#e4e5e9"
+                                                                    }
+                                                                    onMouseOver={() =>
+                                                                        setHoverRating(
+                                                                            ratingValue
+                                                                        )
+                                                                    }
+                                                                    onMouseOut={() =>
+                                                                        setHoverRating(
+                                                                            null
+                                                                        )
+                                                                    }
+                                                                />
+                                                            </label>
+                                                        );
+                                                    }
+                                                )}
+                                            </div>
 
-                                                <Form.Group
-                                                    className="mb-3 "
-                                                    controlId="exampleForm.ControlTextarea1"
-                                                >
-                                                    <Form.Label>
-                                                        Review
-                                                    </Form.Label>
-                                                    <Form.Control
-                                                        {...register("dic", {
-                                                            required: true,
-                                                        })}
-                                                        className="rounded-0"
-                                                        as="textarea"
-                                                        rows={4}
-                                                    />
-                                                </Form.Group>
+                                            <Form.Group
+                                                className="mb-3 "
+                                                controlId="exampleForm.ControlTextarea1"
+                                            >
+                                                <Form.Label>Review</Form.Label>
+                                                <Form.Control
+                                                    {...register("dic", {
+                                                        required: true,
+                                                    })}
+                                                    className="rounded-0"
+                                                    as="textarea"
+                                                    rows={4}
+                                                />
+                                            </Form.Group>
 
-                                                <button
-                                                    type="submit"
-                                                    className="btn btn-danger rounded-0 w-100 py-2"
-                                                >
-                                                    Submit
-                                                </button>
-                                            </Form>
-                                        </div>
+                                            <button
+                                                type="submit"
+                                                className="btn btn-danger rounded-0 w-100 py-2"
+                                            >
+                                                Submit
+                                            </button>
+                                        </Form>
                                     </div>
+                                </div>
+                            </div>
+                            <div className="col-lg-7">
+                                <div className="addReviewBg">
+                                    <img
+                                        className="w-100"
+                                        src={addReviewSectionBg}
+                                        alt=""
+                                    />
                                 </div>
                             </div>
                         </div>
