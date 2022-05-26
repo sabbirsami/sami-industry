f1e236952f5d0bfd6da34b7872d981510430d824
SabbirSami <smd71430@gmail.com>
2022-05-26 13:22:43 +0600

    fix my profile component

--- src/Components/Dashboard/MyProfile.js
+++ src/Components/Dashboard/MyProfile.js
@@ -17,32 +17,26 @@ const MyProfile = () => {
                     <div className="divider mx-auto bg-danger mb-4"></div>
                     <div className="row ">
                         <div className=" col-lg-3">
-                            <div className="user_photo p-4 ms-lg-auto">
-                                <img
-                                    src={photoURL || blankProfile}
-                                    className="card-img-top w-100 rounded-0"
-                                    alt="..."
-                                />
+                            <div className="user_photo shadow p-2 m-lg-4 ms-lg-auto">
+                                <div className="img_field">
+                                    <img
+                                        src={photoURL || blankProfile}
+                                        className="card-img-top w-100 rounded-0"
+                                        alt="..."
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <div className="col-lg-9">
+                        <div className="col-lg-7">
                             <div className="card-body me-lg-auto">
                                 <h5 className="card-title">{}</h5>
-                                <div className="name_field position-relative">
+                                <div className="name_field">
                                     <p className="m-0">
                                         <b>
                                             <small>Name:</small>
                                         </b>
                                     </p>
                                     <h2 className="fw-light">{displayName}</h2>
-                                    <button className="btn btn-outline-secondary text-dark rounded-0 position-absolute top-0 end-0">
-                                        <CustomLink
-                                            className="d-block"
-                                            to="/dashboard/editProfile"
-                                        >
-                                            Edit Profile
-                                        </CustomLink>
-                                    </button>
                                 </div>
                                 <div className="email_field">
                                     <p className="m-0">
@@ -72,6 +66,16 @@ const MyProfile = () => {
                                     <small>{}</small>
                                 </p>
                             </div>
+                        </div>
+                        <div className="col-lg-2">
+                            <button className="btn btn-outline-secondary text-dark rounded-0 mt-lg-4">
+                                <CustomLink
+                                    className="d-block"
+                                    to="/dashboard/editProfile"
+                                >
+                                    Edit Profile
+                                </CustomLink>
+                            </button>
                         </div>
                     </div>
                 </div>
