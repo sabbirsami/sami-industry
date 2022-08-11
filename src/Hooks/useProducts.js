762accd07f902393957cce630300771fa2ad8a60
SabbirSami <smd71430@gmail.com>
2022-08-11 16:48:09 +0600

    add custom loading for product

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 
 const useProducts = () => {
     const [products, setProducts] = useState([]);
+    const [productLoading, setProductLoading] = useState(true);
     useEffect(() => {
         axios.get("https://samindustry.herokuapp.com/product").then((data) => {
             const newData = data.data;
             setProducts(newData);
+            setProductLoading(false);
         });
     }, []);
-    return [products, setProducts];
+    return [products, setProducts, productLoading];
 };
 export default useProducts;
